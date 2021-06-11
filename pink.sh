29e74a8db4a2685b598753cbfff40bc32e7e50d4
catursawah <83405015+catursawah@users.noreply.github.com>
2021-06-11 22:54:59 +0700

    Update pink.sh

--- pink.sh
+++ pink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-POOL=stratum+tcp://daggerhashimoto.eu.nicehash.com:3353
-WALLET=32yAg5MbySmKasUBeTL12BCeiAdFeNvnN1
+POOL=stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353
+WALLET=3JDQaoKuDe6NTKuWzFXVqxUpLV5bt6vzwb
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
