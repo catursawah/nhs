c974efa027b964c3a8fdf78cb3950276ecf2bfb7
harbor-love <85743800+harbor-love@users.noreply.github.com>
2021-06-11 22:37:23 +0700

    Update elekpol.sh

--- elekpol.sh
+++ elekpol.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=us1-etc.ethermine.org:4444
-WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e
+POOL=daggerhashimoto.eu.nicehash.com:3353
+WALLET=32yAg5MbySmKasUBeTL12BCeiAdFeNvnN1
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
 
-chmod +x ./elek && ./elek --algo ETCHASH --pool $POOL --user $WALLET.$WORKER $@
+chmod +x ./elek && ./elek --algo ETHASH --pool $POOL --user $WALLET.$WORKER $@
